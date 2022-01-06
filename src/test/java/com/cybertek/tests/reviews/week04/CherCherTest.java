e7dc6bcf7dea075a0c9300ed8ae010434ee57746
coezen73 <85904903+coezen73@users.noreply.github.com>
2022-01-06 14:39:00 +0100

    First commit

--- src/test/java/com/cybertek/tests/reviews/week04/CherCherTest.java
+++ src/test/java/com/cybertek/tests/reviews/week04/CherCherTest.java
@@ -1,4 +1,11 @@
 package com.cybertek.tests.reviews.week04;
 
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class CherCherTest {
+
+    WebDriver driver;
 }
